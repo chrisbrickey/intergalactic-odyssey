d05037dd6f5a1095a2a731b3435e1f2725d59291
Chris Brickey <cbrickey1@gmail.com>
2025-02-01 16:40:03 +0100

    Add user selection of first galaxy to visit in odyssey

--- src/odyssey.py
+++ src/odyssey.py
@@ -24,6 +24,10 @@ class Odyssey():
 
         # TODO on target_galaxy_count: As the possible size of a universe grows, add logic to control the number of galaxies that the user must explore.
         # e.g. target = proportion of size of universe (unless universe is small)
+
+        # TODO (future development): Prompt user for difficulty level (easy, medium, high).
+        #  This changes the number (or proportion) of galaxies that must be visited.
+
         target_galaxy_count = len(new_universe.scenes)
         intro_message = f"\n\nYour mission is to explore {target_galaxy_count} galaxies, accumulate new technolgies, and return home safely to share what you've learned."
         print(intro_message)
@@ -37,11 +41,33 @@ class Odyssey():
             # Galaxy 1: <name>
             # Galaxy 2: <name>
 
-        # TODO: Allow user to choose starting point; For now default to first galaxy
         next_scene_index = survey[0][0]
-        print(f"\n\nBuckle up! You're headed to Galaxy {next_scene_index + 1}.")
+
+        # Watch out for off-by-one. User is working with figures that represent index + 1.
+        user_selection = input("Enter the number of the galaxy you wish to explore first:  ")
+        minimum = survey[0][0] + 1
+        maximum = survey[-1][0] + 1
+        if self.validate_input_text_to_int(user_selection, minimum, maximum):
+            next_scene_index = int(user_selection) - 1
+        else:
+            print("\nYour entry is not valid so your itinerary has been selected for you.")
+
+        print(f"\nBuckle up! You're headed to Galaxy {next_scene_index + 1}.")
 
         return next_scene_index
+
+    @staticmethod
+    def validate_input_text_to_int(input_string, min, max):
+        is_valid = False
+
+        try:
+            input_int = int(input_string)
+            if input_int in range(min, max + 1):
+                is_valid = True
+        except ValueError:
+            pass
+
+        return is_valid
 
     @classmethod
     def get_instance_count(cls):
