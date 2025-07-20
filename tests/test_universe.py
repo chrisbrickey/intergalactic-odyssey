16f67bd44e8c646ecfa685fa2139c12356fffdb8
Chris Brickey <cbrickey1@gmail.com>
2025-07-20 21:10:36 +0200

    Update Universe tests to avoid testing implementation

--- tests/test_universe.py
+++ tests/test_universe.py
@@ -12,33 +12,15 @@ def new_universe():
    # Teardown
    new_universe.value = None
 
-def test_attributes_on_creation(new_universe):
+def test_scenes_attribute_on_creation(new_universe):
   assert hasattr(new_universe, 'scenes')
-  assert hasattr(new_universe, 'galactic_survey')
-  assert type(new_universe.scenes) == list
-  assert type(new_universe.galactic_survey) == list
-
-def test_create_scenes(new_universe):
-  scenes = new_universe.create_scenes()
-  assert type(scenes) == list
-  assert len(scenes) > 0
-  for element in scenes:
-      assert type(element) == Scene
-
-def test_create_galactic_survey(new_universe):
-  # Arrange
   scenes = new_universe.scenes
+  assert len(scenes) > 0
+  assert all(isinstance(element, Scene) for element in scenes)
 
-  # Act
-  survey = new_universe.create_galactic_survey()
-
-  # Validate structure [[0, scene_name0], [1, scene_name1]]
-  assert type(survey) == list
+def test_survey_attribute_on_creation(new_universe):
+  assert hasattr(new_universe, 'galactic_survey')
+  survey = new_universe.galactic_survey
   assert len(survey) == len(new_universe.scenes)
-
-  for i, element in enumerate(survey):
-    assert type(element) == list
-    assert type(element[0]) == int
-    assert type(element[1]) == str
-    assert survey[i][0] == i
-    assert survey[i][1] == scenes[i].name
+  assert type(survey[0][0]) == int
+  assert type(survey[0][1]) == str
